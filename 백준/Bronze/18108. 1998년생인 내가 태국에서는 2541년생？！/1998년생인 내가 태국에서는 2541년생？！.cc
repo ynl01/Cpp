b5b5db04ef79e73d19c750225130100c6a3ecd1d
ynl01 <129723122+ynl01@users.noreply.github.com>
2024-06-20 22:55:34 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x = 2541 - 1998;
+
+    int a;
+
+    cin >> a;
+
+    int y = a - x;
+
+    cout << y << endl;
+
+    return 0;
+}
