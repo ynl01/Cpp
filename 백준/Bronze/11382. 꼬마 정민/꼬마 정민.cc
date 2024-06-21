8139162d98b331fdf66dcd78ce8a72397d2f7685
ynl01 <129723122+ynl01@users.noreply.github.com>
2024-06-21 23:43:29 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long a, b, c;
+
+    cin >> a >> b >> c;
+
+
+    long long num = a + b + c;
+
+    cout << num << endl;
+
+    return 0;
+}
