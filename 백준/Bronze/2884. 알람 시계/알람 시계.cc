591183e8468aada17dc5670777998a8550dfe614
ynl01 <129723122+ynl01@users.noreply.github.com>
2024-07-01 01:07:09 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int H, M;
+    cin >> H >> M;
+
+    M -= 45;
+    if (M < 0) {
+        M += 60;
+        H--;
+        if (H < 0) {
+            H = 23;
+        }
+    }
+
+    cout << H << " " << M << endl;
+
+    return 0;
+}
